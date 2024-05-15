71dd1c68a2bcefb47cb78186831efee13ffbabc2
afablelorenz <afablelorenz@gmail.com>
2024-05-15 20:18:29 +0200

    updated script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const list=['pane','frutta','verdura','cipolle','pasta','formaggio'];
 let i=0;
+const ulElem=document.querySelector('ul');
 
 while (i < list.length){
-    
+    const element= document.createElement('li');
+    element.append(list[i]);
+
+    ulElem.appendChild(element);
+    i++;
 }
